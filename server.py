5c46d5ba1508a365899a1a200ceff9b3665d3e49
Madison May <madison@indico.io>
2014-11-16 23:49:59 +0100

    Fine tuning

--- server.py
+++ server.py
@@ -7,27 +7,42 @@ from indicoio import text_tags
 app = Flask(__name__)
 app.debug = True
 
-DEFAULT = "http://www.reddit.com/.rss"
+feed = "http://www.reddit.com/.rss"
 
 def thresholded(tags, minimum):
+	""" 
+	Remove all tags with probability less than `minimum` 
+	"""
 	return dict((category, prob) for category, prob in tags.items()
 	        	if prob > minimum)
 
-def likely_category(tags, minimum=0.1):
+def likely_tag(tags, minimum=0.1):
+	""" 
+	Threshold tags, then get the tag with the highest probability.
+	If no tag probability exceeds the minimum, return the string 'none'
+	"""
 	trimmed = thresholded(tags, minimum) or {'none': 0}
 	return max(trimmed, key=lambda key: trimmed[key])
 
 def parsed(entry):
+	"""
+	Strip unnecessary content from the return of feedparser, 
+	and augment with the output of indico's `text_tags` API
+	"""
 	return {
 		'title': entry['title'],
 		'link': entry['link'],
-		'tag': likely_category(text_tags(entry['title']))
+		'tag': likely_tag(text_tags(entry['title']))
 	}
 
 @app.route('/')
 def main():
+	# simple threading to prevent network calls from blocking
 	pool = ThreadPool(16)
-	entries = pool.map(parsed, feedparser.parse(DEFAULT)['entries'])
+	entries = feedparser.parse(feed)['entries']
+	entries = pool.map(parsed, entries)
+
+	# render template with additional jinja2 data
 	return render_template('main.html', entries=entries)
 
 if __name__ == '__main__':
