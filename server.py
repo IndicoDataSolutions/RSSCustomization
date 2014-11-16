9aebe03fe678b2dec98b72ea2ea5446208061af3
Madison May <madison@indico.io>
2014-11-16 22:19:21 +0100

    Beginning to work with feedparser

--- server.py
+++ server.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 import feedparser
 
 app = Flask(__name__)
+app.debug = True
+
+DEFAULT = "http://www.reddit.com/subreddits/new.rss"
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+	entries = feedparser.parse(DEFAULT)['entries']
+	entries = [(entry['title'], entry['link']) for entry in entries]
+	return render_template('main.html')
 
 if __name__ == '__main__':
     app.run()
